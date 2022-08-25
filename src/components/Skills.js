ff262a049faf42489396a1cc876b32a32e9db5db
gizem28 <gizemodun28@gmail.com>
2022-08-25 13:42:19 +0200

    last commit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,8 +8,8 @@ import css from "../assets/img/css3.svg";
 import sass from "../assets/img/sass.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
+// import arrow1 from "../assets/img/arrow1.svg";
+// import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 import colorSharp2 from "../assets/img/color-sharp2.png";
 
